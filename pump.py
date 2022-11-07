eb0e9dc35f0feab7ef59dcfd5d4a2bc641a042b7
kokohandoko00 <63644640+kokohandoko00@users.noreply.github.com>
2022-11-07 14:10:01 +0700

    add if else on pump code

--- pump.py
+++ pump.py
@@ -3,16 +3,27 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-motor =4
-
-GPIO.setup(4, GPIO.OUT) 
-GPIO.output(4, GPIO.HIGH)
+tds=1000
+pH=7
 
 while(True):
   try:
-    GPIO.output(4, GPIO.LOW)
-    print("ONE")
-    time.sleep(2)
+    if tds<=1000:
+      GPIO.setup(17, GPIO.OUT) 
+      GPIO.output(17, GPIO.HIGH)
+      GPIO.output(17, GPIO.LOW)
+      print("ONE")
+      time.sleep(2)
+    if pH>=9:
+      GPIO.setup(27, GPIO.OUT) 
+      GPIO.output(27, GPIO.HIGH)
+      GPIO.output(27, GPIO.LOW)
+      GPIO.setup(22, GPIO.OUT) 
+      GPIO.output(22, GPIO.HIGH)
+      GPIO.output(22, GPIO.LOW)
+      print("TWO")
+      time.sleep(2)
+
 
   except KeyboardInterupt:
    break
