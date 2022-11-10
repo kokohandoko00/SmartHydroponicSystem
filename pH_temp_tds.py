7a3ac38950b223dcaf9290a586fe0af4d585b80e
kokohandoko00 <63644640+kokohandoko00@users.noreply.github.com>
2022-11-10 16:14:56 +0700

    fix pH_temp_tds

--- pH_temp_tds.py
+++ pH_temp_tds.py
@@ -31,28 +31,21 @@ GPIO.setmode(GPIO.BCM)
 def pump(ppm,base):
     if ppm<=1050:
       GPIO.setup(17, GPIO.OUT) 
-      GPIO.output(17, GPIO.HIGH)
       GPIO.output(17, GPIO.LOW)
-      #print("ONE")
-      time.sleep(2) #agar pompa hanya bisa berjalan setiap 10 menit pendeteksian
-    if base>=7:
+      time.sleep(2)
+      GPIO.output(17, GPIO.HIGH)
+      print("ONE")
+      #GPIO.cleanup()
+    if base>=9:
       #case if two relay channel activated
       GPIO.setup(27, GPIO.OUT) 
-      GPIO.output(27, GPIO.HIGH)
       GPIO.output(27, GPIO.LOW)
-      GPIO.setup(22, GPIO.OUT) 
-      GPIO.output(22, GPIO.HIGH)
-      GPIO.output(22, GPIO.LOW)
-      #print("TWO")
-      time.sleep(2) #agar pompa hanya bisa berjalan setiap 10 menit pendeteksian
-      #case if only one relay channel activated. with this option, it should have configured on wire
-      # GPIO.setup(27, GPIO.OUT) 
-      # GPIO.output(27, GPIO.HIGH)
-      # GPIO.output(27, GPIO.LOW)
-      # print("TWO")
-      # time.sleep(2)
+      time.sleep(1) 
+      GPIO.output(27, GPIO.HIGH)
+      print("TWO")
+      # GPIO.cleanup()
     #if KeyboardInterrupt:
-    #  GPIO.cleanup()
+      #  GPIO.cleanup()
       
 def read_temp_raw():
     f = open(device_file, 'r')
