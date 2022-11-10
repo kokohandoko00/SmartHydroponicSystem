54ac1416ff61f9c59b92a7c46ef600ba5c167e6b
kokohandoko00 <63644640+kokohandoko00@users.noreply.github.com>
2022-11-10 16:06:31 +0700

    trial pump code

--- pump.py
+++ pump.py
@@ -3,40 +3,67 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-tds=1000
-pH=8
+
 
 # ph=7
 # tds=1050<=tds<=1400
 
-while(True):
-  try:
-    if tds<=1050:
-      GPIO.setup(17, GPIO.OUT) 
-      GPIO.output(17, GPIO.LOW)
-      time.sleep(200)
-      GPIO.output(17, GPIO.HIGH)
-      print("ONE")
+# while(True):
+#   try:
+#     if tds<=1050:
+#       GPIO.setup(17, GPIO.OUT) 
+#       GPIO.output(17, GPIO.LOW)
+#       time.sleep(200)
+#       GPIO.output(17, GPIO.HIGH)
+#       print("ONE")
+#       #GPIO.cleanup()
+#     if pH>=9:
+#       #case if two relay channel activated
+#       GPIO.setup(27, GPIO.OUT) 
+#       GPIO.output(27, GPIO.LOW)
+#       time.sleep(1) 
+#       GPIO.output(27, GPIO.HIGH)
+#       #GPIO.setup(22, GPIO.OUT) 
+#       #GPIO.output(22, GPIO.HIGH)
+#       #GPIO.output(22, GPIO.LOW)
+#       print("TWO")
+#       GPIO.cleanup()
+#       #case if only one relay channel activated. with this option, it should have configured on wire
+#       # GPIO.setup(27, GPIO.OUT) 
+#       # GPIO.output(27, GPIO.HIGH)
+#       # GPIO.output(27, GPIO.LOW)
+#       # print("TWO")
+#       # time.sleep(2)
+
+
+#   except KeyboardInterrupt:
+#    GPIO.cleanup()
+#    break
+
+def pump(ppm,base):
+  if ppm<=1050:
+    GPIO.setup(17, GPIO.OUT) 
+    GPIO.output(17, GPIO.LOW)
+    time.sleep(2)
+    GPIO.output(17, GPIO.HIGH)
+    print("ONE")
       #GPIO.cleanup()
-    if pH>=9:
+  if base>=9:
       #case if two relay channel activated
-      GPIO.setup(27, GPIO.OUT) 
-      GPIO.output(27, GPIO.LOW)
-      time.sleep(1) 
-      GPIO.output(27, GPIO.HIGH)
-      #GPIO.setup(22, GPIO.OUT) 
-      #GPIO.output(22, GPIO.HIGH)
-      #GPIO.output(22, GPIO.LOW)
-      print("TWO")
-      GPIO.cleanup()
-      #case if only one relay channel activated. with this option, it should have configured on wire
-      # GPIO.setup(27, GPIO.OUT) 
-      # GPIO.output(27, GPIO.HIGH)
-      # GPIO.output(27, GPIO.LOW)
-      # print("TWO")
-      # time.sleep(2)
+    GPIO.setup(27, GPIO.OUT) 
+    GPIO.output(27, GPIO.LOW)
+    time.sleep(1) 
+    GPIO.output(27, GPIO.HIGH)
+    print("TWO")
+    # GPIO.cleanup()
 
+def read_sensor():
+  while True:
+    tds=1000
+    pH=8
+    pump(tds,pH)
+    time.sleep(2)
 
-  except KeyboardInterrupt:
-   GPIO.cleanup()
-   break
+while True:
+ print(read_sensor())
+ time.sleep(1)
