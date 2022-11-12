24a9e25e82df1832bb2ecdd81da431b5b7ff719f
kokohandoko00 <63644640+kokohandoko00@users.noreply.github.com>
2022-11-12 14:03:00 +0700

    completed lcd code

--- lcd.py
+++ lcd.py
@@ -3,4 +3,15 @@ from rpi_lcd import LCD
 
 lcd =LCD()
 
-
+def safe_exit(signum, frame):
+    exit(1)
+try:
+    signal(SIGTERM, safe_exit)
+    signal(SIGHUP, safe_exit)
+    lcd.text("Hello,", 1)
+    lcd.text("Raspberry Pi!", 2)
+    pause()
+except KeyboardInterrupt:
+    pass
+finally:
+    lcd.clear()
