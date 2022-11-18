a9a166edc42ede9956b17e973a63aefdbd9fa6f9
kokohandoko00 <63644640+kokohandoko00@users.noreply.github.com>
2022-11-18 17:42:02 +0700

    modify lamp code

--- lamp.py
+++ lamp.py
@@ -1,9 +1,13 @@
 import RPi.GPIO as GPIO
 import time
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-GPIO.setmode(GPIO.BCM)
+def lamp_on():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.OUT) 
+    GPIO.output(17, GPIO.LOW)
+    time.sleep(2)
+    GPIO.output(17, GPIO.HIGH)
 
-GPIO.setup(17, GPIO.OUT) 
-GPIO.output(17, GPIO.LOW)
-time.sleep(2)
-GPIO.output(17, GPIO.HIGH)
+scheduler = BlockingScheduler()
+scheduler.reschedule_job('lamp_on', trigger='cron', minute='018***')
