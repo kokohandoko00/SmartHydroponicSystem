6737447241fc00322ec2d9e2b12738278dde9e69
Ahmad Zidan <4zidan@gmail.com>
2022-11-24 18:20:25 +0700

    fix channel ads

--- capstone/main.py
+++ capstone/main.py
@@ -33,7 +33,7 @@ class SmartHydroponic(object):
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.ads = ADS.ADS1115(self.i2c)
         self.lcd = LCD(bus=3)
-        self.channel_0 = AnalogIn(self.ads, ADS.P3)
+        self.channel_0 = AnalogIn(self.ads, ADS.P0)
         self.channel_1 = AnalogIn(self.ads, ADS.P1)
         self.lamp_state = False
 
